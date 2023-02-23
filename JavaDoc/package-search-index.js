65f642801b7b028d53a2923e556bc810936a1ec6
londero-lorenzo <126183761+londero-lorenzo@users.noreply.github.com>
2023-02-23 17:01:33 +0100

    add JavaDoc files

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"arduino"},{"l":"database"},{"l":"file_managers"},{"l":"interfaccia"},{"l":"interfaces"}];updateSearchResults();
